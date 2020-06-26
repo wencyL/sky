5a4ead7810f5a384f292cbe7816ec3ec7371e837
wencyL <935446689@qq.com>
2020-06-26 17:00:12 +0800

    初始化

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+    entry: "./src/index.tsx",
+    output: {
+        filename: "bundle.js",
+        path: __dirname + "/dist"
+    },
+
+    devtool: "source-map",
+    devServer: {
+        host: '127.0.0.1',
+        port: '3001',
+        proxy: { // proxy URLs to backend development server
+            '/api': 'http://localhost:3000'
+        },
+        contentBase: path.join(__dirname, 'public'), // boolean | string | array, static file location
+        compress: true, // enable gzip compression
+        historyApiFallback: true, // true for index.html upon 404, object for multiple paths
+        hot: true, // hot module replacement. Depends on HotModuleReplacementPlugin
+        https: false, // true for self-signed, object for cert authority
+        noInfo: true, // only errors & warns on hot reload
+        // ...
+    },
+
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".json"]
+    },
+
+    module: {
+        rules: [
+            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+
+            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+        ]
+    },
+
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    }
+};
